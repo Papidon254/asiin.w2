b3a107fd527e083593b55f056cf5db1b0c486c95
Dominick Odhiambo <esco@students.uonbi.ac.ke>
2024-02-29 17:02:44 +0300

    First commit

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+#list =['abcd', 786, 2.23, 'John', 70.2]
+#tinylist = ['John', 123]
+#print(tinylist)
+#print(list[2:3])
+#print(list + tinylist)
+#print(tinylist * 3)
+#tuple = 'abcd', 768, 2.223, 'John', 70.3
+#print(tuple) 
+#print(tuple[1:3])
+#print(tuple * 2)
+#tinytuple = 'Wed', 'Sun', 33.34
+#print(tinytuple)
+#print(tuple + tinytuple)
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+tinydict = {'name': 'John', 'code' :6732, 'dept' :4354}
+print(dict['one'])
